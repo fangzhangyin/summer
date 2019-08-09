c484646915431b593da2ba57063917a4e2066d67
Administrator <2570413931@qq.com>
2019-08-09 11:15:18 +0800

    广播+自定义定时关闭应用

--- broadcast/src/main/java/com/example/broadcast/finall.java
+++ broadcast/src/main/java/com/example/broadcast/finall.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,7 +19,16 @@ public class finall extends baseActivity {
 
     @BindView(R.id.exit0)
     Button exit0;
+    @BindView(R.id.canel)
+    Button canel;
+    private Timer timer;
+    private long secound;
 
+    @BindView(R.id.back)
+    Button back;
+
+    @BindView(R.id.time)
+    EditText time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,20 +37,41 @@ public class finall extends baseActivity {
         exit0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-                //开启广播，定时关闭整个app
-                Timer timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        //do something
-                        Intent intent = new Intent();
-                        intent.setAction(EXIT_APP_ACTION);
-                        sendBroadcast(intent);
+                String s=time.getText().toString();
+                if(s!=null&&!s.equals("")){
+                    secound=Long.parseLong(s);
+                    secound=secound*1000;
+                    //开启广播，定时关闭整个app
+                    if(timer==null){
+                        timer=new Timer();
+                        timer.schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                //do something
+                                Intent intent = new Intent();
+                                intent.setAction(EXIT_APP_ACTION);
+                                sendBroadcast(intent);
+                            }
+                        }, secound);//延时执行
                     }
-                }, 2000);//延时1s执行
+                }
 
-
+            }
+        });
+        canel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(timer!=null){
+                    timer.cancel();
+                    timer.purge();
+                    timer=null;
+                }
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
